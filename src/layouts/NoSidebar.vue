73ed5820f8385c22ae4ef6878b25d6aff27f3c0e
stxphxn <991266+stxphxn@users.noreply.github.com>
2020-04-27 14:06:00 +0100

    add no sidebar layout

---
+++ src/layouts/NoSidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout
+    view="hHh LpR fFf"
+    class="bg-accent"
+  >
+    <q-page-container
+      class="text-negative"
+    >
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'NoSidebar',
+};
+</script>
