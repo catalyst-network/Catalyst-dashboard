0d7ab3a728bc40ec06bf5879eeff05de769a06b3
stxphxn <991266+stxphxn@users.noreply.github.com>
2020-04-27 14:32:21 +0100

    create setup page

---
+++ src/pages/Setup/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="q-pa-md">
+    <div
+      v-if="!loading"
+      class="column justify-center q-gutter-y-md"
+    >
+      Dashboard Setup
+    </div>
+    <q-inner-loading
+      dark
+      :showing="loading"
+    >
+      <q-spinner
+        size="75px"
+        color="secondary"
+      />
+    </q-inner-loading>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'Setup',
+  computed: {
+    ...mapState({
+      loading: (state => state.Settings.loading),
+    }),
+  },
+};
+</script>
+<style lang="stylus" scoped>
+
+.q-inner-loading--dark
+  background $primary
+
+</style>
