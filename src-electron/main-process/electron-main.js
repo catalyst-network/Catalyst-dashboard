eb82350043c49a91a02157cee218d24fd0a80f21
stxphxn <991266+stxphxn@users.noreply.github.com>
2020-04-27 16:51:49 +0100

    debug electron devtools on wsl

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -1,8 +1,8 @@
-import { app, BrowserWindow, nativeTheme } from 'electron'
+import { app, BrowserWindow, nativeTheme } from 'electron';
 
 try {
   if (process.platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
-    require('fs').unlinkSync(require('path').join(app.getPath('userData'), 'DevTools Extensions'))
+    require('fs').unlinkSync(require('path').join(app.getPath('userData'), 'DevTools Extensions'));
   }
 } catch (_) { }
 
@@ -11,12 +11,13 @@ try {
  * The reason we are setting it here is that the path needs to be evaluated at runtime
  */
 if (process.env.PROD) {
-  global.__statics = require('path').join(__dirname, 'statics').replace(/\\/g, '\\\\')
+  global.__statics = require('path').join(__dirname, 'statics').replace(/\\/g, '\\\\');
 }
 
-let mainWindow
+let mainWindow;
+let devtools = null;
 
-function createWindow () {
+function createWindow() {
   /**
    * Initial window options
    */
@@ -31,26 +32,31 @@ function createWindow () {
 
       // More info: /quasar-cli/developing-electron-apps/electron-preload-script
       // preload: path.resolve(__dirname, 'electron-preload.js')
-    }
-  })
+    },
+  });
 
-  mainWindow.loadURL(process.env.APP_URL)
+  devtools = new BrowserWindow();
+
+  mainWindow.loadURL(process.env.APP_URL);
+  mainWindow.webContents.setDevToolsWebContents(devtools.webContents);
+  mainWindow.webContents.openDevTools({ mode: 'detach' });
+
 
   mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
